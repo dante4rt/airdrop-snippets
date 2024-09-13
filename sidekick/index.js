da635a7de047ce4f196135e11075fe7f37fb25e7
Dante4rt <rxmxdhxni@gmail.com>
2024-09-13 22:46:42 +0700

    feat: add Sidekick

---
+++ sidekick/index.js
@@ -0,0 +1,67 @@
+const _0x53d100 = _0x2d66;
+function _0x2d66(e, t) {
+  const n = _0xafe0();
+  return (_0x2d66 = function (e, t) {
+    return (e -= 249), n[e];
+  })(e, t);
+}
+function clickElement(e) {
+  var t = _0x53d100,
+    n = new MouseEvent(t(250), { view: window, bubbles: !0, cancelable: !0 });
+  e[t(251)](n);
+}
+(function () {
+  for (var e = _0x2d66, t = _0xafe0(); ; )
+    try {
+      if (
+        936337 ==
+        +parseInt(e(254)) * (-parseInt(e(260)) / 2) +
+          -parseInt(e(258)) / 3 +
+          parseInt(e(264)) / 4 +
+          parseInt(e(252)) / 5 +
+          (parseInt(e(253)) / 6) * (-parseInt(e(263)) / 7) +
+          -parseInt(e(256)) / 8 +
+          (parseInt(e(255)) / 9) * (parseInt(e(262)) / 10)
+      )
+        break;
+      t.push(t.shift());
+    } catch (e) {
+      t.push(t.shift());
+    }
+})(),
+  alert(_0x53d100(259));
+const buttons = document.querySelectorAll(_0x53d100(257));
+let counter = 0,
+  clicked = !1;
+function _0xafe0() {
+  const e = [
+    '.w-full.text-black.flex.items-center',
+    '78948JdpIBg',
+    'Sidekick Airdrop Snippet by HappyCuanAirdrop \n\nSubscribe: https://t.me/HappyCuanAirdrop',
+    '254bExHed',
+    'The button has been clicked ',
+    '13730MpHjXu',
+    '7zZfaka',
+    '5123876GdKmwO',
+    'log',
+    'click',
+    'dispatchEvent',
+    '3040650NytvtR',
+    '2198802itFqQj',
+    '13962gLWTnk',
+    '18747mqSpga',
+    '13174112IyOJrH',
+  ];
+  return (_0xafe0 = function () {
+    return e;
+  })();
+}
+buttons.forEach((e) => {
+  var t = _0x53d100;
+  clickElement(e),
+    counter++,
+    console[t(249)](t(261) + counter + ' time(s).'),
+    (clicked = !0);
+}),
+  (clicked && 0 !== counter) ||
+    console[_0x53d100(249)]('All buttons have been clicked.');
