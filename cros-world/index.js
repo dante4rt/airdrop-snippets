81f61b6df9961fbb33ededabc02f97da42ab83c3
Dante4rt <rxmxdhxni@gmail.com>
2024-10-12 17:24:51 +0700

    feat: add Cros World

---
+++ cros-world/index.js
@@ -0,0 +1,79 @@
+const _0x5b0ac2 = _0x517b;
+function _0x517b(t, e) {
+  const n = _0x1381();
+  return (_0x517b = function (t, e) {
+    return (t -= 111), n[t];
+  })(t, e);
+}
+function clickElement(t) {
+  var e = new MouseEvent(_0x517b(115), {
+    view: window,
+    bubbles: !0,
+    cancelable: !0,
+  });
+  t.dispatchEvent(e);
+}
+(function () {
+  for (var t = _0x517b, e = _0x1381(); ; )
+    try {
+      if (
+        321269 ==
+        +parseInt(t(122)) +
+          (parseInt(t(111)) / 2) * (parseInt(t(116)) / 3) +
+          (parseInt(t(119)) / 4) * (-parseInt(t(126)) / 5) +
+          -parseInt(t(113)) / 6 +
+          (-parseInt(t(118)) / 7) * (parseInt(t(112)) / 8) +
+          (-parseInt(t(124)) / 9) * (-parseInt(t(117)) / 10) +
+          parseInt(t(121)) / 11
+      )
+        break;
+      e.push(e.shift());
+    } catch (t) {
+      e.push(e.shift());
+    }
+})(),
+  alert(
+    'Cros World Snippet by HappyCuanAirdrop \nSubscribe: https://t.me/HappyCuanAirdrop'
+  );
+const listItems = document[_0x5b0ac2(123)]('li');
+function _0x1381() {
+  const t = [
+    '10141307xGrEyU',
+    '83722pPGKBJ',
+    'querySelectorAll',
+    '2718UfEsXK',
+    ' time(s).',
+    '135EaTJSO',
+    'All tasks completed! \nSubscribe: https://t.me/HappyCuanAirdrop',
+    'forEach',
+    'Button clicked ',
+    '179518IPxuUT',
+    '267224NVRBYT',
+    '3475020RTLLFS',
+    'No buttons were found. If the buttons are inside an iframe, please click inside the iframe first, then try running the script again.',
+    'click',
+    '18LYnoJW',
+    '13070BEAWIm',
+    '119txOwqx',
+    '69724byCFid',
+    '.submit.complete-website-click.update-website-url',
+  ];
+  return (_0x1381 = function () {
+    return t;
+  })();
+}
+let counter = 0,
+  clicked = !1;
+listItems[_0x5b0ac2(128)]((t) => {
+  const n = _0x5b0ac2,
+    e = t[n(123)](n(120));
+  0 < e.length &&
+    e[n(128)]((t) => {
+      var e = n;
+      clickElement(t),
+        counter++,
+        console.log(e(129) + counter + e(125)),
+        (clicked = !0);
+    });
+}),
+  clicked && 0 !== counter ? alert(_0x5b0ac2(127)) : alert(_0x5b0ac2(114));
