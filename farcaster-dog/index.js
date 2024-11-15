8d5ce460c2f1751ed99d642e2741c0ef8029a756
Dante4rt <rxmxdhxni@gmail.com>
2024-11-15 09:20:36 +0700

    feat: add Farcaster Dog

---
+++ farcaster-dog/index.js
@@ -0,0 +1,84 @@
+var _0x5d5407 = _0x3f08;
+(function (_0x29f3ef, _0x4cb335) {
+  var _0x17bb31 = _0x3f08,
+    _0x38b940 = _0x29f3ef();
+  while (!![]) {
+    try {
+      var _0x3b5ac7 =
+        parseInt(_0x17bb31(0x120)) / 0x1 +
+        (-parseInt(_0x17bb31(0x11d)) / 0x2) *
+          (parseInt(_0x17bb31(0x11c)) / 0x3) +
+        (-parseInt(_0x17bb31(0x119)) / 0x4) *
+          (parseInt(_0x17bb31(0x11a)) / 0x5) +
+        (parseInt(_0x17bb31(0x121)) / 0x6) *
+          (-parseInt(_0x17bb31(0x125)) / 0x7) +
+        parseInt(_0x17bb31(0x124)) / 0x8 +
+        parseInt(_0x17bb31(0x118)) / 0x9 +
+        parseInt(_0x17bb31(0x128)) / 0xa;
+      if (_0x3b5ac7 === _0x4cb335) break;
+      else _0x38b940['push'](_0x38b940['shift']());
+    } catch (_0x267306) {
+      _0x38b940['push'](_0x38b940['shift']());
+    }
+  }
+})(_0xbf49, 0x7dcf9);
+function clickElement(_0x2275d1) {
+  var _0xf31806 = _0x3f08,
+    _0x215d55 = new MouseEvent(_0xf31806(0x129), {
+      view: window,
+      bubbles: !0x0,
+      cancelable: !0x0,
+    });
+  _0x2275d1[_0xf31806(0x11f)](_0x215d55);
+}
+function _0xbf49() {
+  var _0x8d7bf2 = [
+    '\x20time(s).',
+    'The\x20button\x20has\x20been\x20clicked\x20',
+    '6625935pxIBBw',
+    '772848xDtPJy',
+    '5LUlUOV',
+    'All\x20tasks\x20done!\x0a\x0aSubscribe:\x20https://t.me/HappyCuanAirdrop',
+    '2967093ZkzKdo',
+    '2qYXejQ',
+    'forEach',
+    'dispatchEvent',
+    '584794NbsYtG',
+    '6DIuMzY',
+    'log',
+    'li\x20button.button-color:first-of-type',
+    '4942304bfdVnn',
+    '4086712wjYbzI',
+    'length',
+    'Farcaster\x20Dog\x20Airdrop\x20Snippet\x20by\x20HappyCuanAirdrop\x20\x0a\x0aSubscribe:\x20https://t.me/HappyCuanAirdrop',
+    '3425830PLkNkd',
+    'click',
+  ];
+  _0xbf49 = function () {
+    return _0x8d7bf2;
+  };
+  return _0xbf49();
+}
+alert(_0x5d5407(0x127));
+const buttons = document['querySelectorAll'](_0x5d5407(0x123));
+function _0x3f08(_0x419cad, _0x46d60a) {
+  var _0xbf4985 = _0xbf49();
+  return (
+    (_0x3f08 = function (_0x3f084d, _0x50fec3) {
+      _0x3f084d = _0x3f084d - 0x117;
+      var _0x3949be = _0xbf4985[_0x3f084d];
+      return _0x3949be;
+    }),
+    _0x3f08(_0x419cad, _0x46d60a)
+  );
+}
+let counter = 0x0;
+0x0 === buttons['length']
+  ? console[_0x5d5407(0x122)]('No\x20buttons\x20found.')
+  : (buttons[_0x5d5407(0x11e)]((_0x17a21d) => {
+      var _0xcdb0a5 = _0x5d5407;
+      clickElement(_0x17a21d),
+        counter++,
+        console['log'](_0xcdb0a5(0x117) + counter + _0xcdb0a5(0x12a));
+    }),
+    counter === buttons[_0x5d5407(0x126)] && alert(_0x5d5407(0x11b)));
